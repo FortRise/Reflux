c944294e3a2f96948a091aeedcab9c11f6f4b049
Terria-K <teuria@proton.me>
2025-04-10 02:08:25 +0800

    First commit!

---
+++ README.md
@@ -0,0 +1,15 @@
+# Reflux
+
+A simple Harmony-like patching library using [MonoMod.RuntimeDetour](https://github.com/MonoMod/MonoMod).
+
+A goal of this project is to almost mimic how Harmony patches the methods, though some features might not be added due to
+lack of interest with it.
+
+## Roadmap
++ [x] Prefixes
++ [x] Postfixes
++ [ ] Finalizers
++ [ ] Reverse Patchers
+
+## Things will not do
++ [ ] Transpilers
---
+++ example/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.CompilerServices;
+using RefluxLibrary;
+
+var reflux = new Reflux();
+
+// reflux.Patch(typeof(MakeMePatch).GetMethod("ToPatchWith")!, prefix: ToPatchWith_Prefix_Another);
+reflux.Patch(typeof(MakeMePatch).GetMethod("ToPatchWith")!, prefix: ToPatchWith_Prefix, postfix: ToPatchWith_Postfix, finalizer: ToPatchWith_Finalizer);
+
+var patching = new MakeMePatch();
+patching.ToPatchWith("OKKK");
+MakeMePatch.SomeStatic();
+Console.WriteLine(patching.Add(4, 2));
+
+
+// static void ToPatchWith_Prefix_Another(ref string name, MakeMePatch __instance) 
+// {
+//     name = "Will do";
+// }
+
+static bool ToPatchWith_Prefix(ref string name, MakeMePatch __instance) 
+{
+    if (name == "OKKK")
+    {
+        return true;
+    }
+    Console.WriteLine(name);
+    return false;
+}
+
+static void ToPatchWith_Postfix(ref string name, MakeMePatch __instance) 
+{
+    Console.WriteLine("Postfix called!");
+}
+
+static void ToPatchWith_Finalizer(Exception __exception) 
+{
+
+}
+
+
+public class MakeMePatch 
+{
+    private int hello;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void ToPatchWith(string name)
+    {
+        hello = 5;
+        Console.WriteLine(hello);
+        Console.WriteLine(name + "ORIG");
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public int Add(int a, int b)
+    {
+        return (a + b);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void SomeStatic() 
+    {
+        Console.WriteLine("Im static");
+    }
+}
